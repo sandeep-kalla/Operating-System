ab5dc129f251d5a26c3bcd304e9920286fbf336b
sandeep-kalla <dark1178rider@gmail.com>
2024-10-23 14:53:53 +0530

    Update BankersAlgorithm.cpp

--- BankersAlgorithm.cpp
+++ BankersAlgorithm.cpp
@@ -1,63 +1,63 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-class Banker {
-    int p, r;
-    vector<vector<int>> max, alloc, need;
-    vector<int> avail;
+class BankersAlgorithm {
+    int numProcesses, numResources;
+    vector<vector<int>> allocation, max, need;
+    vector<int> available;
 public:
-    Banker(int np, int nr) : p(np), r(nr), max(np, vector<int>(nr)), 
-        alloc(np, vector<int>(nr)), need(np, vector<int>(nr)), avail(nr) {}
-    
-    void setup(vector<vector<int>>& m, vector<vector<int>>& a, vector<int>& v) {
-        max = m; alloc = a; avail = v;
-        for(int i = 0; i < p; i++)
-            for(int j = 0; j < r; j++)
-                need[i][j] = max[i][j] - alloc[i][j];
+    BankersAlgorithm(int p, int r)
+        : numProcesses(p), numResources(r), allocation(p, vector<int>(r)),
+          max(p, vector<int>(r)), need(p, vector<int>(r)), available(r) {}
+    void inputResources() {
+        cout << "Enter allocation matrix:\n";
+        for (auto& row : allocation) for (auto& elem : row) cin >> elem;
+        cout << "Enter maximum matrix:\n";
+        for (auto& row : max) for (auto& elem : row) cin >> elem;
+        cout << "Enter available resources:\n";
+        for (auto& res : available) cin >> res;
+        calculateNeed();
     }
-    
+    void calculateNeed() {
+        for (int i = 0; i < numProcesses; ++i)
+            for (int j = 0; j < numResources; ++j)
+                need[i][j] = max[i][j] - allocation[i][j];
+    }
     bool isSafe() {
-        vector<bool> fin(p);
-        vector<int> work = avail;
+        vector<bool> finish(numProcesses, false);
+        vector<int> work = available, safeSequence;
         int count = 0;
-        
-        while(count < p) {
+        while (count < numProcesses) {
             bool found = false;
-            for(int i = 0; i < p && !found; i++) {
-                if(!fin[i]) {
-                    int j;
-                    for(j = 0; j < r && need[i][j] <= work[j]; j++);
-                    if(j == r) {
-                        for(j = 0; j < r; j++) work[j] += alloc[i][j];
-                        fin[i] = found = true;
-                        count++;
-                    }
+            for (int p = 0; p < numProcesses; ++p) {
+                if (!finish[p] && canAllocate(p, work)) {
+                    for (int r = 0; r < numResources; ++r) work[r] += allocation[p][r];
+                    safeSequence.push_back(p);
+                    finish[p] = true;
+                    found = true;
+                    count++;
                 }
             }
-            if(!found) return false;
+            if (!found) return false;
         }
+        cout << "System is in a safe state.\nSafe sequence is: ";
+        for (int i : safeSequence) cout << i << " ";
+        cout << endl;
         return true;
     }
-    
-    bool request(int pid, vector<int>& req) {
-        for(int i = 0; i < r; i++)
-            if(req[i] > need[pid][i] || req[i] > avail[i]) return false;
-            
-        vector<int> temp_avail = avail;
-        vector<vector<int>> temp_alloc = alloc, temp_need = need;
-        
-        for(int i = 0; i < r; i++) {
-            avail[i] -= req[i];
-            alloc[pid][i] += req[i];
-            need[pid][i] -= req[i];
-        }
-        
-        if(isSafe()) return true;
-        
-        avail = temp_avail;
-        alloc = temp_alloc;
-        need = temp_need;
-        return false;
+private:
+    bool canAllocate(int p, const vector<int>& work) const {
+        for (int r = 0; r < numResources; ++r)
+            if (need[p][r] > work[r]) return false;
+        return true;
     }
 };
+int main() {
+    int processes, resources;
+    cout << "Enter number of processes and resources: ";
+    cin >> processes >> resources;
+    BankersAlgorithm ba(processes, resources);
+    ba.inputResources();
+    if (!ba.isSafe()) cout << "System is not in a safe state.\n";
+    return 0;
+}
